7a81311fd73418a3b8c108059155ac4938a77170
JIBIN G JIJI <102510014+Jibin-G-Jiji@users.noreply.github.com>
2026-05-21 11:24:08 +0530

    Update tests.py

--- django_todo/todos/tests.py
+++ django_todo/todos/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Todo
 
-# Create your tests here.
+
+class TodoModelTest(TestCase):
+    def test_create_todo(self):
+        todo = Todo.objects.create(title="Test task")
+        self.assertEqual(str(todo), "Test task")
+        self.assertFalse(todo.completed)
+
+    def test_index_page_loads(self):
+        response = self.client.get(reverse("todos:index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_todo(self):
+        response = self.client.post(reverse("todos:index"), {"title": "New task"})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Todo.objects.count(), 1)
+
+    def test_toggle_todo(self):
+        todo = Todo.objects.create(title="Toggle me")
+        self.client.post(reverse("todos:toggle", args=[todo.pk]))
+        todo.refresh_from_db()
+        self.assertTrue(todo.completed)
+
+    def test_delete_todo(self):
+        todo = Todo.objects.create(title="Delete me")
+        self.client.post(reverse("todos:delete", args=[todo.pk]))
+        self.assertEqual(Todo.objects.count(), 0)
